83171b936c4985053ca08a4a5d13e342428cffab
Fisphame <1489923239@qq.com>
2025-11-23 15:45:34 +0800

    Add files via upload

---
+++ client/ClientModEvents.java
@@ -0,0 +1,18 @@
+package com.pha.trainees.client;
+
+import com.pha.trainees.Main;
+import com.pha.trainees.registry.ModEntities;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Main.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientModEvents {
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(ModEntities.CALLED_SWORD.get(), CalledSwordRenderer::new);
+        event.registerEntityRenderer(ModEntities.KUN_TRAINEES.get(), KunTraineesRenderer::new);
+        event.registerEntityRenderer(ModEntities.KUN_ANTI.get(), KunAntiRenderer::new);
+    }
+}
